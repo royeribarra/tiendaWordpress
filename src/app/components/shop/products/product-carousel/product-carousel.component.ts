e56edd54aaadec5d1df4cf1dce0e17cc5dc2fbbc
royeribarra <royeribarrao@gmail.com>
2021-09-24 14:47:50 -0500

    cantidad de imagenes

--- src/app/components/shop/products/product-carousel/product-carousel.component.ts
+++ src/app/components/shop/products/product-carousel/product-carousel.component.ts
@@ -10,81 +10,81 @@ import { ProductService } from 'src/app/components/shared/services/product.servi
 import { WishlistService } from 'src/app/components/shared/services/wishlist.service';
 
 @Component({
-  selector: 'app-product-carousel',
-  templateUrl: './product-carousel.component.html',
-  styleUrls: ['./product-carousel.component.sass']
+    selector: 'app-product-carousel',
+    templateUrl: './product-carousel.component.html',
+    styleUrls: ['./product-carousel.component.sass']
 })
 export class ProductCarouselComponent implements OnInit {
-  @Output() onOpenProductDialog: EventEmitter<any> = new EventEmitter();
-  @Input('product') product: Array<Product> = [];
-  public config: SwiperConfigInterface = {};
-  contentLoaded = false;
-  constructor(private dialog: MatDialog, private router: Router, private cartService: CartService, private productService: ProductService, private wishlistService: WishlistService) { }
+    @Output() onOpenProductDialog: EventEmitter<any> = new EventEmitter();
+    @Input('product') product: Array<Product> = [];
+    public config: SwiperConfigInterface = {};
+    contentLoaded = false;
+    constructor(private dialog: MatDialog, private router: Router, private cartService: CartService, private productService: ProductService, private wishlistService: WishlistService) { }
 
-  ngOnInit() {
-    setTimeout(() => {
-      this.contentLoaded = true;
-    }, 3000);
-  }
-  ngAfterViewInit(){ 
-    this.config = {
-      observer: true,
-      slidesPerView: 5,
-      spaceBetween: 16,
-      keyboard: true,
-      navigation: true,
-      pagination: false,
-      grabCursor: true,
-      loop: true,
-      preloadImages: false,
-      lazy: true,
-      breakpoints: {
-        480: {
-          slidesPerView: 1
-        },
-        740: {
-          slidesPerView: 2,
-        },
-        960: {
-          slidesPerView: 3,
-        },
-        1280: {
-          slidesPerView: 4,
-        },
-
-
-      }
+    ngOnInit() {
+        setTimeout(() => {
+        this.contentLoaded = true;
+        }, 3000);
     }
-  }
+    ngAfterViewInit(){ 
+        this.config = {
+            observer: true,
+            slidesPerView: 4,
+            spaceBetween: 16,
+            keyboard: true,
+            navigation: true,
+            pagination: false,
+            grabCursor: true,
+            loop: true,
+            preloadImages: false,
+            lazy: true,
+            breakpoints: {
+                480: {
+                slidesPerView: 1
+                },
+                740: {
+                slidesPerView: 2,
+                },
+                960: {
+                slidesPerView: 3,
+                },
+                1280: {
+                slidesPerView: 4,
+                },
 
 
-  public openProductDialog(product){
-    let dialogRef = this.dialog.open(ProductDialogComponent, {
-        data: product,
-        panelClass: 'product-dialog',
-    });
-    dialogRef.afterClosed().subscribe(product => {
-      if(product){
-        this.router.navigate(['/products', product.id, product.name]);
-      }
-    });
-  }
+            }
+        }
+    }
+
+
+    public openProductDialog(product){
+        let dialogRef = this.dialog.open(ProductDialogComponent, {
+            data: product,
+            panelClass: 'product-dialog',
+        });
+        dialogRef.afterClosed().subscribe(product => {
+        if(product){
+            this.router.navigate(['/products', product.id, product.name]);
+        }
+        });
+    }
 
    // Add to cart
-   public addToCart(product: Product,  quantity: number = 1) {
-    this.cartService.addToCart(product,quantity);
-    console.log(product, quantity);
-  }
+    public addToCart(product: Product,  quantity: number = 1) {
+        this.cartService.addToCart(product,quantity);
+        console.log(product, quantity);
+    }
 
-   // Add to wishlist
-   public addToWishlist(product: Product) {
-    this.wishlistService.addToWishlist(product);
- }
+    // Add to wishlist
+    public addToWishlist(product: Product) {
+        this.wishlistService.addToWishlist(product);
+    }
 
-    // Add to compare
+        // Add to compare
     public addToCompare(product: Product) {
-      this.productService.addToCompare(product);
-   }
+        this.productService.addToCompare(product);
+    }
 }
 
 
