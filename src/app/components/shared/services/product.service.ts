43874b665ae6ffafcea9080100a84652b2a99ec9
royeribarra <royeribarrao@gmail.com>
2021-09-24 15:19:20 -0500

    buscando ando

--- src/app/components/shared/services/product.service.ts
+++ src/app/components/shared/services/product.service.ts
@@ -5,130 +5,126 @@ import { Product } from 'src/app/modals/product.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { map } from 'rxjs/operators';
 
-
-
 // Get product from Localstorage
 let products = JSON.parse(localStorage.getItem("compareItem")) || [];
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductService {
-  public currency : string = 'USD';
-  public catalogMode : boolean = false;
+    public currency : string = 'USD';
+    public catalogMode : boolean = false;
 
-  private _url: string = "assets/data/";
-  public url = "assets/data/banners.json";
+    private _url: string = "assets/data/";
+    public url = "assets/data/banners.json";
 
-  public compareProducts : BehaviorSubject<Product[]> = new BehaviorSubject([]);
-  public observer   :  Subscriber<{}>;
+    public compareProducts : BehaviorSubject<Product[]> = new BehaviorSubject([]);
+    public observer   :  Subscriber<{}>;
 
-  constructor(private httpClient: HttpClient, public snackBar: MatSnackBar) {
-   this.compareProducts.subscribe(products => products = products)
-  }
+    constructor(
+        private httpClient: HttpClient, 
+        public snackBar: MatSnackBar
+    ) {
+        this.compareProducts.subscribe(products => products = products)
+    }
 
-  private products(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>('assets/data/products2.json');
-  }
+    private products(): Observable<Product[]> {
+        return this.httpClient.get<Product[]>('assets/data/products2.json');
+    }
 
-  public banners(): Observable<any[]>{
-    return this.httpClient.get<any[]>(this.url);
-  }
+    public banners(): Observable<any[]>{
+        return this.httpClient.get<any[]>(this.url);
+    }
 
-
-    // Get Banners
+        // Get Banners
     public getBanners() {
-      return this.banners();
+        return this.banners();
     }
 
-    // Get Banners
+        // Get Banners
     public getProducts(): Observable<Product[]> {
-      return this.products();
+        return this.products();
     }
 
+        // Get Products By Id
+    public getProduct(id: number): Observable<Product> {
+        return this.products().pipe(map(items => {
+        return items.find((item: Product) =>
+            { return item.id === id; });
+        }));
+        // return this.products.find(product=> product.id === id);
 
-      // Get Products By Id
-  public getProduct(id: number): Observable<Product> {
-    return this.products().pipe(map(items => {
-      return items.find((item: Product) =>
-        { return item.id === id; });
-      }));
-    // return this.products.find(product=> product.id === id);
+        // return this.httpClient.get<Product>(this._url + 'product-' + id + '.json');
+    }
+            /*
+        ---------------------------------------------
+        ----------  Compare Product  ----------------
+        ---------------------------------------------
+    */
 
-    // return this.httpClient.get<Product>(this._url + 'product-' + id + '.json');
-  }
+    // Get Compare Products
+    public getComapreProducts(): Observable<Product[]> {
+        const itemsStream = new Observable(observer => {
+            observer.next(products);
+            observer.complete();
+        });
+        return <Observable<Product[]>>itemsStream;
+    }
 
+    // If item is aleready added In compare
+    public hasProduct(product: Product): boolean {
+        const item = products.find(item => item.id === product.id);
+        return item !== undefined;
+    }
 
-        /*
-      ---------------------------------------------
-      ----------  Compare Product  ----------------
-      ---------------------------------------------
-   */
+    // Get Products By Slug
+    public getProductBySlug(slug: string): Observable<Product> {
+        return this.products().pipe(map(items => { 
+            return items.find((item: any) => { 
+                return item.name.replace(' ', '-') === slug; 
+            }); 
+        }));
+    }
 
-// Get Compare Products
-public getComapreProducts(): Observable<Product[]> {
-  const itemsStream = new Observable(observer => {
-    observer.next(products);
-    observer.complete();
-  });
-  return <Observable<Product[]>>itemsStream;
-}
+    // Add to compare
+    public addToCompare(product: Product): Product | boolean {
+        let message, status;
+        var item: Product | boolean = false;
+        if (this.hasProduct(product)) {
+            item = products.filter(item => item.id === product.id)[0];
+            const index = products.indexOf(item);
+            this.snackBar.open('The product  ' + product.name + ' already added to comparison list.', '×', { panelClass: 'error', verticalPosition: 'top', duration: 3000 });
 
-// If item is aleready added In compare
-public hasProduct(product: Product): boolean {
-  const item = products.find(item => item.id === product.id);
-  return item !== undefined;
-}
+        } else {
+            if(products.length < 4)
+            products.push(product);
+            message = 'The product ' + product.name + ' has been added to comparison list.';
+            status = 'success';
+            this.snackBar.open(message, '×', { panelClass: [status], verticalPosition: 'top', duration: 3000 });
 
-  // Get Products By Slug
-  public getProductBySlug(slug: string): Observable<Product> {
-    return this.products().pipe(map(items => { 
-      return items.find((item: any) => { 
-        return item.name.replace(' ', '-') === slug; 
-      }); 
-    }));
-  }
+        }
+            localStorage.setItem("compareItem", JSON.stringify(products));
+            return item;
+    }
 
- // Add to compare
- public addToCompare(product: Product): Product | boolean {
-  let message, status;
-  var item: Product | boolean = false;
-  if (this.hasProduct(product)) {
-    item = products.filter(item => item.id === product.id)[0];
-    const index = products.indexOf(item);
-    this.snackBar.open('The product  ' + product.name + ' already added to comparison list.', '×', { panelClass: 'error', verticalPosition: 'top', duration: 3000 });
+    // Removed Product
+    public removeFromCompare(product: Product) {
+        if (product === undefined) { return; }
+        const index = products.indexOf(product);
+        products.splice(index, 1);
+        localStorage.setItem("compareItem", JSON.stringify(products));
+    }
 
-  } else {
-    if(products.length < 4)
-      products.push(product);
-      message = 'The product ' + product.name + ' has been added to comparison list.';
-      status = 'success';
-      this.snackBar.open(message, '×', { panelClass: [status], verticalPosition: 'top', duration: 3000 });
+    // Get Products By category
+    public getProductByCategory(category: string): Observable<Product[]> {
+        return this.products().pipe(map(items =>
+            items.filter((item: Product) => {
+                if(category == 'all')
+                    return item
+                    else
+                    return item.category === category;
 
-  }
-    localStorage.setItem("compareItem", JSON.stringify(products));
-    return item;
-}
-
-// Removed Product
-public removeFromCompare(product: Product) {
-  if (product === undefined) { return; }
-  const index = products.indexOf(product);
-  products.splice(index, 1);
-  localStorage.setItem("compareItem", JSON.stringify(products));
-}
-
-   // Get Products By category
-   public getProductByCategory(category: string): Observable<Product[]> {
-    return this.products().pipe(map(items =>
-       items.filter((item: Product) => {
-         if(category == 'all')
-            return item
-            else
-            return item.category === category;
-
-       })
-     ));
-  }
-
+            })
+        ));
+    }
 }
